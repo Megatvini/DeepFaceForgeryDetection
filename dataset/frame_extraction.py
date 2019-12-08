809399feffa226b2a7cc291cf00ea176b21b9ae1
Nika Doghonadze <ndogh13@freeuni.edu.ge>
2019-12-08 22:32:08 +0100

    Make frame extraction run in parallel

--- dataset/frame_extraction.py
+++ dataset/frame_extraction.py
@@ -1,14 +1,18 @@
 import argparse
 import os
+from concurrent.futures import ProcessPoolExecutor
 from os.path import join
 
 import cv2
 from tqdm import tqdm
 
 
-def extract_frames(data_path, output_path, file_prefix):
+def extract_frames(inp):
+    data_path, output_path, file_prefix = inp
+
     os.makedirs(output_path, exist_ok=True)
     reader = cv2.VideoCapture(data_path)
+    length = int(reader.get(cv2.CAP_PROP_FRAME_COUNT))
     frame_num = 0
     while reader.isOpened():
         success, image = reader.read()
@@ -17,6 +21,7 @@ def extract_frames(data_path, output_path, file_prefix):
         cv2.imwrite(join(output_path, '{}{:04d}.jpg'.format(file_prefix, frame_num)), image)
         frame_num += 1
     reader.release()
+    return length
 
 
 def extract_images(videos_path, out_path, num_videos):
@@ -24,13 +29,19 @@ def extract_images(videos_path, out_path, num_videos):
 
     video_files = os.listdir(videos_path)
     print('total videos found - {}, extracting from - {}'.format(len(video_files), min(len(video_files), num_videos)))
+    video_files = video_files[:num_videos]
 
-    for index, video_file in tqdm(enumerate(video_files[:num_videos])):
-        video_file_name = video_file.split('.')[0]
-        video_out_path = os.path.join(out_path, video_file_name)
-        video_path = os.path.join(videos_path, video_file)
-        file_prefix = '{}_'.format(video_file_name)
-        extract_frames(video_path, video_out_path, file_prefix=file_prefix)
+    def get_video_input_output_pairs():
+        for index, video_file in enumerate(video_files):
+            video_file_name = video_file.split('.')[0]
+            v_out_path = os.path.join(out_path, video_file_name)
+            v_path = os.path.join(videos_path, video_file)
+            f_prefix = '{}_'.format(video_file_name)
+            yield v_path, v_out_path, f_prefix
+
+    executor = ProcessPoolExecutor()
+    results = list(tqdm(executor.map(extract_frames, get_video_input_output_pairs()), total=len(video_files)))
+    print('total frames extracted: ', sum(results))
 
 
 def parse_args():
