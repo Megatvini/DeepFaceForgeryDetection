407e38180603312110d6e91945e1303c6c608571
Nika Doghonadze <ndogh13@freeuni.edu.ge>
2019-12-17 15:43:36 +0100

    Only extract faces from video frames

--- dataset/frame_extraction.py
+++ dataset/frame_extraction.py
@@ -4,7 +4,60 @@ from concurrent.futures import ProcessPoolExecutor
 from os.path import join
 
 import cv2
+import dlib
 from tqdm import tqdm
+
+face_detector = dlib.get_frontal_face_detector()
+
+
+# generates a quadratic bounding box
+# source: https://github.com/ondyari/FaceForensics/blob/master/classification/detect_from_video.py
+def get_boundingbox(face, width, height, scale=1.3, minsize=None):
+    """
+    Expects a dlib face to generate a quadratic bounding box.
+    :param face: dlib face class
+    :param width: frame width
+    :param height: frame height
+    :param scale: bounding box size multiplier to get a bigger face region
+    :param minsize: set minimum bounding box size
+    :return: x, y, bounding_box_size in opencv form
+    """
+    x1 = face.left()
+    y1 = face.top()
+    x2 = face.right()
+    y2 = face.bottom()
+    size_bb = int(max(x2 - x1, y2 - y1) * scale)
+    if minsize:
+        if size_bb < minsize:
+            size_bb = minsize
+    center_x, center_y = (x1 + x2) // 2, (y1 + y2) // 2
+
+    # Check for out of bounds, x-y top left corner
+    x1 = max(int(center_x - size_bb // 2), 0)
+    y1 = max(int(center_y - size_bb // 2), 0)
+    # Check for too big bb size for given x, y
+    size_bb = min(width - x1, size_bb)
+    size_bb = min(height - y1, size_bb)
+
+    return x1, y1, size_bb
+
+
+def crop_image(image, face, scale=1.3):
+    img_height, img_width, _ = image.shape
+    x, y, size = get_boundingbox(face, img_width, img_height, scale=scale)
+
+    # generate cropped image
+    cropped_face = image[y:y + size, x:x + size]
+    return cropped_face
+
+
+def extract_face(image):
+    detections = face_detector(image, 1)
+    if len(detections) == 0:
+        return None
+
+    d = detections[0]
+    return crop_image(image, d)
 
 
 def extract_frames(inp):
@@ -18,7 +71,11 @@ def extract_frames(inp):
         success, image = reader.read()
         if not success:
             break
-        cv2.imwrite(join(output_path, '{}{:04d}.jpg'.format(file_prefix, frame_num)), image)
+        face_image = extract_face(image)
+        if face_image is not None:
+            print(f'no face found, so skipping {output_path}')
+            out_path = join(output_path, '{}{:04d}.jpg'.format(file_prefix, frame_num))
+            cv2.imwrite(out_path, face_image)
         frame_num += 1
     reader.release()
     return length
