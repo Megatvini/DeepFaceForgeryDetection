7617ddf074fed0c83ae9851169fe6d7c0831b54c
Nika Doghonadze <ndogh13@freeuni.edu.ge>
2020-01-06 03:41:00 +0100

    Add squeezenet to models

--- src/model.py
+++ src/model.py
@@ -13,18 +13,21 @@ class Lambda(nn.Module):
 
 
 class CNN_LSTM(nn.Module):
-    def __init__(self, image_encoding_size=512, hidden_size=512):
+    def __init__(self, image_encoding_size=512, hidden_size=512, pretrained_encoder=True):
         super(CNN_LSTM, self).__init__()
         self.image_encoding_size = image_encoding_size
         self.hidden_size = hidden_size
         self.cnn_encoder = nn.Sequential(
-            torchvision.models.squeezenet1_1(pretrained=True),
+            torchvision.models.squeezenet1_1(pretrained=pretrained_encoder),
             nn.ReLU(),
+            nn.Dropout(0.5),
             nn.Linear(1000, image_encoding_size)
         )
         self.lstm = nn.LSTM(
             image_encoding_size, hidden_size, num_layers=1, bias=True, batch_first=True, bidirectional=True
         )
+        self.relu = nn.ReLU()
+        self.droupout = nn.Dropout(0.5)
         self.fc = nn.Linear(2*hidden_size, 1)
 
     def forward(self, images):
@@ -34,7 +37,7 @@ class CNN_LSTM(nn.Module):
         out, _ = self.lstm(image_encodings)
 
         mid_frame = out[:, depth // 2, :]
-        res = self.fc(mid_frame)
+        res = self.fc(self.droupout(self.relu(mid_frame)))
         return res.squeeze()
 
 
@@ -48,15 +51,30 @@ class ResNet3d(nn.Module):
 
 
 class ResNet2d(nn.Module):
-    def __init__(self, final_hidden_dim=256, dropout=0.5):
+    def __init__(self, final_hidden_dim=256, dropout=0.5, pretrained=True):
         super(ResNet2d, self).__init__()
-        resnet = torchvision.models.resnet18(pretrained=True)
+        resnet = torchvision.models.resnet18(pretrained=pretrained)
         resnet.fc = nn.Linear(512, final_hidden_dim)
         self.model = nn.Sequential(
             resnet,
             nn.ReLU(),
             nn.Dropout(dropout),
             nn.Linear(final_hidden_dim, 1)
+        )
+
+    def forward(self, images):
+        return self.model(images.squeeze()).squeeze()
+
+
+class SqueezeNet2d(nn.Module):
+    def __init__(self, dropout=0.5, pretrained=True):
+        super(SqueezeNet2d, self).__init__()
+        squeeze_net = torchvision.models.squeezenet1_1(pretrained=pretrained)
+        self.model = nn.Sequential(
+            squeeze_net,
+            nn.ReLU(),
+            nn.Dropout(dropout),
+            nn.Linear(1000, 1),
         )
 
     def forward(self, images):
