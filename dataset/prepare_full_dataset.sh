2589ff853fa0b585fc1fa95612fa160d8dd202b1
Nika Doghonadze <ndogh13@freeuni.edu.ge>
2020-01-22 01:28:58 +0100

    Add neural textures dataset preparation script

--- dataset/prepare_full_dataset.sh
+++ dataset/prepare_full_dataset.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "YES" | python faceforensics_download_v4.py -d NeuralTextures -c c40 neural_textures_c40
-echo "YES" | python faceforensics_download_v4.py -d original -c c40 original_c40
+python frame_extraction.py original_c40/original_sequences/youtube/c40/videos/ /mnt/mtcnn/neural_textures_faces_c40 --num_videos 999999
 
-python frame_extraction.py original_c40/original_sequences/youtube/c40/videos/ images_large/original/ --num_videos 999999
-python frame_extraction.py neural_textures_c40/manipulated_sequences/NeuralTextures/c40/videos/ images_large/neural_textures/ --num_videos 999999
+echo "YES" | python faceforensics_download_v4.py -d original -c c40 original_c40
+python frame_extraction.py neural_textures_c40/manipulated_sequences/NeuralTextures/c40/videos/ /mnt/original_faces_c40 --num_videos 999999
